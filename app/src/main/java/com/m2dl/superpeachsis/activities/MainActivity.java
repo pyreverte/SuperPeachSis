3eb3726806d3600d462a49ca6d35b235b37592af
JakeSimmo <jacob.simmonds@master-developpement-logiciel.com>
2021-03-19 10:27:51 +0100

    Add main menu

--- app/src/main/java/com/m2dl/superpeachsis/activities/MainActivity.java
+++ app/src/main/java/com/m2dl/superpeachsis/activities/MainActivity.java
@@ -2,17 +2,34 @@ package com.m2dl.superpeachsis.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.m2dl.superpeachsis.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button newGameButton = findViewById(R.id.new_game_button);
+        newGameButton.setOnClickListener(v -> toGameActivity());
+
+        final Button aboutButton = findViewById(R.id.about_button);
+        // TODO aboutButton.setOnClickListener(v -> toAboutActivity());
+
+        final Button quitButton = findViewById(R.id.quit_button);
+        quitButton.setOnClickListener(v -> System.exit(0));
+    }
+
+    public void toGameActivity() {
+        Intent i = new Intent(this, GameActivity.class);
+        startActivity(i);
     }
 }
